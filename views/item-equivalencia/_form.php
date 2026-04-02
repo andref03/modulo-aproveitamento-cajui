af02146beee81bb1ee3b402e96ad09e68838f3d6
Andre Felipe <andref.lopes@outlook.com>
2026-04-02 15:52:41 -0300

    refactor: enhance form layout and input handling in ItemEquivalencia and SolicitacaoAproveitamento views

--- views/item-equivalencia/_form.php
+++ views/item-equivalencia/_form.php
@@ -1,37 +1,63 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use app\models\DisciplinaIfnmg;
 
 /** @var yii\web\View $this */
 /** @var app\models\ItemEquivalencia $model */
 /** @var yii\widgets\ActiveForm $form */
+
+$disciplinas = ArrayHelper::map(
+    DisciplinaIfnmg::find()->orderBy('nome')->all(),
+    'id',
+    function ($disciplina) {
+        return $disciplina->nome . ' (' . $disciplina->codigo . ')';
+    }
+);
 ?>
 
 <div class="item-equivalencia-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'solicitacao_id')->textInput() ?>
+    <?= $form->field($model, 'solicitacao_id')->hiddenInput()->label(false) ?>
 
-    <?= $form->field($model, 'disciplina_origem_nome')->textInput(['maxlength' => true]) ?>
+    <div class="row">
+        <div class="col-md-8">
+            <?= $form->field($model, 'disciplina_origem_nome')->textInput(['maxlength' => true]) ?>
+        </div>
 
-    <?= $form->field($model, 'disciplina_origem_carga_horaria')->textInput() ?>
-
-    <?= $form->field($model, 'disciplina_origem_ementa')->textarea(['rows' => 6]) ?>
+        <div class="col-md-4">
+            <?= $form->field($model, 'disciplina_origem_carga_horaria')->textInput() ?>
+        </div>
+    </div>
 
     <?= $form->field($model, 'instituicao_origem')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'disciplina_destino_id')->textInput() ?>
+    <?= $form->field($model, 'disciplina_origem_ementa')->textarea(['rows' => 5]) ?>
 
-    <?= $form->field($model, 'parecer')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'disciplina_destino_id')->dropDownList(
+        $disciplinas,
+        ['prompt' => 'Selecione a disciplina do IFNMG']
+    ) ?>
 
-    <?= $form->field($model, 'justificativa')->textarea(['rows' => 6]) ?>
+    <?php if (!$model->isNewRecord): ?>
+        <hr>
+        <h4>Análise do Coordenador</h4>
 
-    <?= $form->field($model, 'data_analise')->textInput() ?>
+        <?= $form->field($model, 'parecer')->dropDownList([
+            'PENDENTE' => 'PENDENTE',
+            'DEFERIDO' => 'DEFERIDO',
+            'INDEFERIDO' => 'INDEFERIDO',
+        ]) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= $form->field($model, 'justificativa')->textarea(['rows' => 4]) ?>
+    <?php endif; ?>
+
+    <div class="form-group mt-3">
+        <?= Html::submitButton('Salvar Item', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
--- views/solicitacao-aproveitamento/_form.php
+++ views/solicitacao-aproveitamento/_form.php
@@ -1,35 +1,57 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use app\models\Estudante;
+use app\models\Coordenador;
 
 /** @var yii\web\View $this */
 /** @var app\models\SolicitacaoAproveitamento $model */
 /** @var yii\widgets\ActiveForm $form */
+
+$estudantes = ArrayHelper::map(Estudante::find()->orderBy('nome')->all(), 'id', 'nome');
+$coordenadores = ArrayHelper::map(Coordenador::find()->orderBy('nome')->all(), 'id', 'nome');
 ?>
 
 <div class="solicitacao-aproveitamento-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'numero_protocolo')->textInput(['maxlength' => true]) ?>
+    <div class="row">
+        <div class="col-md-6">
+            <?= $form->field($model, 'estudante_id')->dropDownList(
+                $estudantes,
+                ['prompt' => 'Selecione o estudante']
+            ) ?>
+        </div>
 
-    <?= $form->field($model, 'estudante_id')->textInput() ?>
+        <div class="col-md-6">
+            <?= $form->field($model, 'coordenador_id')->dropDownList(
+                $coordenadores,
+                ['prompt' => 'Selecione o coordenador (opcional)']
+            ) ?>
+        </div>
+    </div>
 
-    <?= $form->field($model, 'coordenador_id')->textInput() ?>
+    <?php if (!$model->isNewRecord): ?>
+        <div class="row">
+            <div class="col-md-4">
+                <?= $form->field($model, 'numero_protocolo')->textInput(['readonly' => true]) ?>
+            </div>
 
-    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+            <div class="col-md-4">
+                <?= $form->field($model, 'status')->textInput(['readonly' => true]) ?>
+            </div>
 
-    <?= $form->field($model, 'resultado_final')->textInput(['maxlength' => true]) ?>
+            <div class="col-md-4">
+                <?= $form->field($model, 'resultado_final')->textInput(['readonly' => true]) ?>
+            </div>
+        </div>
+    <?php endif; ?>
 
-    <?= $form->field($model, 'data_criacao')->textInput() ?>
-
-    <?= $form->field($model, 'data_envio')->textInput() ?>
-
-    <?= $form->field($model, 'data_finalizacao')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    <div class="form-group mt-3">
+        <?= Html::submitButton('Salvar Solicitação', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
